fe7640d91c2e1fcecf25a42f2a77ed9ec02f5405
“Varun13016” <dmevarun@gmail.com>
2021-11-22 18:57:00 +0530

    BTS-uc1

---
+++ BTS-uc1/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTS_uc1
+{
+    class Node
+    {
+        public int data;
+        public Node LeftNode;
+        public Node RightNode;
+
+        public Node()
+        {
+            this.LeftNode = null;
+            this.RightNode = null;
+        }
+    }
+}
---
+++ BTS-uc1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BTS_uc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree obj1 = new BinaryTree();
+            obj1.Insert(56);
+            obj1.Insert(30);
+            obj1.Insert(70);
+            obj1.TraversePreorder(obj1.root);
+        }
+    }
+}
